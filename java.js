3ea0f300a8bd98d8458c3bc72ec99007906693dd
Xtend Ettisaf <98172341+XtendEttisaf@users.noreply.github.com>
2022-03-15 12:54:52 +0600

    Add files via upload

--- java.js
+++ java.js
@@ -1,6 +1,18 @@
-var a = document.getElementById('submit')
-
-function submit(){
-    alert("Hey, Nice to meet!")
-    
-}
+var form = document.getElementById('sheetdb-form');
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    fetch(form.action, {
+        method: "POST",
+        body: new FormData(document.getElementById("sheetdb-form")),
+    }).then(
+        response => response.json()
+    ).then((html) => {
+        // you can put any JS code here
+        swal({
+            title: "Success!",
+            text: "Your Question has been Submitted Successfully!",
+            icon: "success",
+            button: "Owh Yeah!",
+          });
+    });
+});
